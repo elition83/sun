d47c22e85a66294e273be76952febfed9e0f7ace
elition83 <110487186+elition83@users.noreply.github.com>
2025-02-05 20:14:49 +0300

    Добаваил в CmdPost команду fake

--- app/Console/Commands/CmdPost.php
+++ app/Console/Commands/CmdPost.php
@@ -8,23 +8,9 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CmdPost extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:post {cmd}';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'app:post {cmd} {count?}';
     protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
         $cmd = $this->argument('cmd');
@@ -53,6 +39,23 @@ class CmdPost extends Command
 
             } catch (ModelNotFoundException $e) {
                 $this->error("Пост с ID $id не найден!");
+            }
+        } elseif ($cmd == 'fake') {
+            $count = $this->argument('count') ?? 1;
+            if (!is_numeric($count) || (int) $count <= 0) {
+                $this->error('Количество должно быть положительным числом.');
+                return;
+            } 
+            $count = (int) $count;
+
+            for ($i = 0; $i < $count; $i++) {
+                $post = Post::create([
+                    'title' => fake()->sentence(),
+                    'content' => fake()->text(),
+                    'author' => fake()->name(),
+                    'image_path' => 'null'
+                ]);
+                $this->info("Пост успешно создан с ID: {$post->id} | Title: {$post->title}");
             }
         } else {
             $this->info('Ошика в команде app:post ( all | create | {id поста} )');
